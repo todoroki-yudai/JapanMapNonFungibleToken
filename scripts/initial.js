0c4e9ad9fe0030e2485c2c998b976dbfbf7e91d5
todoroki <todoroki.godai@gmail.com>
2018-03-04 15:20:30 +0900

    add Japanmap js

---
+++ scripts/initial.js
@@ -0,0 +1,56 @@
+myToken = Prefecture.at(Prefecture.address)
+
+myToken.mint(1,"hokkaido",1);
+myToken.mint(2,"aomori",1);
+myToken.mint(3,"iwate",1);
+myToken.mint(4,"miyagi",1);
+myToken.mint(5,"akita",1);
+myToken.mint(6,"yamagata",1);
+myToken.mint(7,"fukushima",1);
+myToken.mint(8,"ibaraki",1);
+myToken.mint(9,"tochigi",1);
+myToken.mint(10,"gumma",1);
+myToken.mint(11,"saitama",1);
+myToken.mint(12,"chiba",1);
+myToken.mint(13,"tokyo",1);
+myToken.mint(14,"kanagawa",1);
+myToken.mint(15,"niigata",1);
+myToken.mint(16,"toyama",1);
+myToken.mint(17,"ishikawa",1);
+myToken.mint(18,"fukui",1);
+myToken.mint(19,"yamanashi",1);
+myToken.mint(20,"nagano",1);
+myToken.mint(21,"gifu",1);
+myToken.mint(22,"shizuoka",1);
+myToken.mint(23,"aichi",1);
+myToken.mint(24,"mie",1);
+myToken.mint(25,"shiga",1);
+myToken.mint(26,"kyoto",1);
+myToken.mint(27,"osaka",1);
+myToken.mint(28,"hyogo",1);
+myToken.mint(29,"nara",1);
+myToken.mint(30,"wakayama",1);
+myToken.mint(31,"tottori",1);
+myToken.mint(32,"shimane",1);
+myToken.mint(33,"okayama",1);
+myToken.mint(34,"hiroshima",1);
+myToken.mint(35,"yamaguchi",1);
+myToken.mint(36,"tokushima",1);
+myToken.mint(37,"kagawa",1);
+myToken.mint(38,"ehime",1);
+myToken.mint(39,"kochi",1);
+myToken.mint(40,"fukuoka",1);
+myToken.mint(41,"saga",1);
+myToken.mint(42,"nagasaki",1);
+myToken.mint(43,"kumamoto",1);
+myToken.mint(44,"oita",1);
+myToken.mint(45,"miyazaki",1);
+myToken.mint(46,"kagoshima",1);
+myToken.mint(47,"okinawa",1);
+
+// myToken.getToken(2);
+// account1 = web3.eth.accounts[1]
+// myToken.snatch(2, {from:web3.eth.accounts[1], value: 5});
+// web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]));
+// myToken.getToken(2);
+// curl -d "{"jsonrpc":"2.0","method":"eth_sendTransaction","params": [{"from":"0x198e13017d2333712bd942d8b028610b95c363da", "to":"0xa93b8C89d89dc5fe774141eF959A71629EefbdF9", "value": 1e18}], "id":1}" -X POST http://127.0.0.1:8545/
